e3a10bc9da1e4377fac16feff94f8d13840ba4fc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2023-06-07 14:46:19 +0000

    Add try/except for send data points

--- custom_components/tuya_ble/tuya_ble/tuya_ble.py
+++ custom_components/tuya_ble/tuya_ble/tuya_ble.py
@@ -1274,24 +1274,28 @@ class TuyaBLEDevice:
         data = bytearray()
         for dp_id in datapoint_ids:
             dp = self._datapoints[dp_id]
-            value = dp._get_value()
-            print(
-                "{}: Sending datapoint update, id: {}, type: {}: value: {}".format(
+            try:
+                value = dp._get_value()
+                print(
+                    "{}: Sending datapoint update, id: {}, type: {}: value: {}".format(
+                        self.address, dp.id, dp.type.name, dp.value
+                    )
+                )
+                _LOGGER.debug(
+                    "%s: Sending datapoint update, id: %s, type: %s: value: %s",
                     self.address,
                     dp.id,
                     dp.type.name,
-                    dp.value
+                    dp.value,
                 )
-            )
-            _LOGGER.debug(
-                "%s: Sending datapoint update, id: %s, type: %s: value: %s",
-                self.address,
-                dp.id,
-                dp.type.name,
-                dp.value,
-            )
-            data += pack(">BBB", dp.id, int(dp.type.value), len(value))
-            data += value
+                data += pack(">BBB", dp.id, int(dp.type.value), len(value))
+                data += value
+            except:
+                _LOGGER.error(
+                    "%s: Failed to get value of datapoint %s",
+                    self.address,
+                    dp.id,
+                )
 
         await self._send_packet(TuyaBLECode.FUN_SENDER_DPS, data)
 
