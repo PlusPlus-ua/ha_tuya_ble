f47027ae936578c71bda677f436339c0a1267111
ppcsite <72654341+ppcsite@users.noreply.github.com>
2024-02-09 08:46:30 +0200

    Update config_flow.py
    Fixing an issue after updating to Home Assistant 2024.2.0 update

--- custom_components/tuya_ble/config_flow.py
+++ custom_components/tuya_ble/config_flow.py
@@ -28,13 +28,8 @@ from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowHandler, FlowResult
 
 from homeassistant.components.tuya.const import (
-    CONF_ACCESS_ID,
-    CONF_ACCESS_SECRET,
     CONF_APP_TYPE,
-    CONF_AUTH_TYPE,
     CONF_ENDPOINT,
-    SMARTLIFE_APP,
-    TUYA_COUNTRIES,
     TUYA_RESPONSE_CODE,
     TUYA_RESPONSE_MSG,
     TUYA_RESPONSE_SUCCESS,
@@ -45,6 +40,11 @@ from .tuya_ble import SERVICE_UUID, TuyaBLEDeviceCredentials
 
 from .const import (
     DOMAIN,
+    CONF_ACCESS_ID,
+    CONF_ACCESS_SECRET,
+    CONF_AUTH_TYPE,
+    SMARTLIFE_APP,
+    TUYA_SMART_APP,
 )
 from .devices import TuyaBLEData, get_device_readable_name
 from .cloud import HASSTuyaBLEDeviceManager
